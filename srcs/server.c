8cb332a8d896cdcd41447383ff72dda60980daef
goncalofgalmeida <102439759+goncalofgalmeida@users.noreply.github.com>
2025-03-16 00:17:06 +0000

    WIP Recieve length first and malloc once

--- srcs/server.c
+++ srcs/server.c
@@ -5,72 +5,106 @@
 #include <stdlib.h>
 #include <stdio.h> // apagar quando já não precisar
 
-void	append_char_to_msg(char c, char **msg_ptr)
+// Flag to track phase of signal conversion
+int	g_recieved_msg_len = 0;
+
+typedef struct s_state {
+	int		length;
+	int		len_bit_index;
+	int		msg_bit_index;
+	int		current_char;
+	char	*buffer;
+} t_state;
+
+// Process the first 32 bits sent by the client
+void	process_length_bit(int signum, int *bit_index, int *length)
 {
-	char	*c_to_str;
-	char	*temp;
-
-	c_to_str = (char *)malloc(2 * sizeof(char));
-	if (!c_to_str)
-		exit(1);
-	c_to_str[0] = c;
-	c_to_str[1] = '\0';
-	if (*msg_ptr == NULL)
-		*msg_ptr = ft_strdup(c_to_str);
-	else
-	{
-		temp = *msg_ptr;
-		*msg_ptr = ft_strjoin(temp, c_to_str);
-		free(temp);
-	}
-	free(c_to_str);
-}
-
-void	handle_complete_msg(char character, char **message, int *bit_index)
-{
-	if (character == '\0')
-	{
-		ft_putstr_fd(*message, 1);
-		ft_putchar_fd('\n', 1);
-		free(*message);
-		*message = NULL;
-	}
-	else
-		append_char_to_msg(character, message);
-	*bit_index = 7;
-}
-
-void	perform_sig_processing(int signum, int *flag)
-{
-	static int	bit_index;
-	static char	character;
-	static char	*message;
-
-	if (*flag == 0)
-	{
-		bit_index = 7;
-		character = 0;
-		message = NULL;
-		*flag = 1;
-	}
 	if (signum == SIGUSR1)
-		character |= (1 << bit_index);
+	{
+		*length |= (1 << *bit_index);
+	}
 	else
-		character &= ~(1 << bit_index);
-	// enviar aqui o sinal para o client de que pode continuar, provavelmente atraves de sa_flags > sa_siginfo > siginfo_t->si_pid
-	bit_index--;
+	{
+		*length &= ~(1 << *bit_index);
+	}
+	(*bit_index)--;
+	if (*bit_index < 0)
+		g_recieved_msg_len = *length;
+}
+
+void	alloc_message_buffer(char **message, int length)
+{
+	*message = (char*)malloc((length + 1) * sizeof(char));
+	if (!*message)
+		exit(1);
+	(*message)[length] = '\0';
+	
+}
+
+void	process_char_bit(int signum, int *bit_index, char *character)
+{
+	if (signum == SIGUSR1)
+		*character |= (1 << *bit_index);
+	else
+		*character &= ~(1 << *bit_index);
+	(*bit_index)--;
+}
+
+void	append_char_to_msg(char character, int current_index, char **msg_ptr)
+{
+	(*msg_ptr)[current_index] = character;
+}
+
+void	handle_complete_msg(char **message)
+{
+	ft_putstr_fd(*message, 1);
+	ft_putchar_fd('\n', 1);
+	free(*message);
+	g_recieved_msg_len = 0;
+	*message = NULL;
+}
+
+void	process_length(int signum, char **message)
+{
+	static int	bit_index = 31;
+	static int	message_length = 0;
+
+	process_length_bit(signum, &bit_index, &message_length);
+	if (g_recieved_msg_len > 0)
+	{
+		printf("Recieved the len: %i\n", message_length);
+		alloc_message_buffer(message, message_length);
+	}
+}
+void	process_message(int signum, char **message)
+{
+	static int	bit_index = 7;
+	static char	character = 0;
+	static int	current_char_index = 0;
+
+	process_char_bit(signum, &bit_index, &character);
 	if (bit_index < 0)
 	{
-		handle_complete_msg(character, &message, &bit_index);
+		append_char_to_msg(character ,current_char_index, message);
+		current_char_index++;
 		character = 0;
+		if (current_char_index == g_recieved_msg_len)
+		{
+			handle_complete_msg(message);
+			current_char_index = 0;
+		}
+		bit_index = 7;
 	}
 }
 
 void	handle_signal(int signum)
 {
-	static int	flag;
+	static char	*message = NULL;
 
-	perform_sig_processing(signum, &flag);
+	if (g_recieved_msg_len == 0)
+		process_length(signum, &message);
+	else
+		process_message(signum, &message);
 }
 
 int	main(void)
